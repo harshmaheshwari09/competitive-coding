ec7b6067c18b2b894070c7181d80977367adfcd8
Harsh Maheshwari <bittz09@gmail.com>
2023-06-10 22:49:13 +0100

    adding sol for 1351

---
+++ src/Leetcode1351/Solution.java
@@ -0,0 +1,19 @@
+package Leetcode1351;
+
+//https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int ans = 0;
+        int i = grid.length - 1;
+        int j = 0;
+        while (i >= 0 && j < grid[0].length) {
+            if (grid[i][j] < 0) {
+                ans += grid[0].length - j;
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+}
