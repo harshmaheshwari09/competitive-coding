cc062c4bcfc74eb1d642a5a5b1e85d7745dbb113
Harsh Maheshwari <bittz09@gmail.com>
2023-06-16 00:23:19 +0100

    adding sol for problem #0155

---
+++ src/leetcode/problem0155/SolutionTest.java
@@ -0,0 +1,19 @@
+package leetcode.problem0155;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    @Test
+    public void testSolution1() {
+        MinStack st = new MinStack();
+        st.push(-2);
+        st.push(0);
+        st.push(-3);
+        Assertions.assertEquals(-3, st.getMin());
+        st.pop();
+        Assertions.assertEquals(0, st.top());
+        Assertions.assertEquals(-2, st.getMin());
+
+    }
+}
