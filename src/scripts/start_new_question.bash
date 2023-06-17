2231ae5a03c1dbf18b24fa6b425f85f8e67c6e22
Harsh Maheshwari <bittz09@gmail.com>
2023-06-17 01:51:15 +0100

    adding sol for problem #0322

--- src/scripts/start_new_question.bash
+++ src/scripts/start_new_question.bash
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "Please provide an integer as an argument."
-  exit 1
-fi
+url="$1"
+number=$2
 
-cd ~/IdeaProjects/Leetcode/src/leetcode
+# Extract the last part of the URL as the filename
+filename=$(basename "$url")
 
-input=$1
-paddedNumber=$(printf "%04d" "$input")
+# Remove any file extension from the filename
+filename="${filename%.*}"
+
+# Replace dashes with spaces
+filename="${filename//-/ }"
+
+# Convert the filename parts to have the first letter uppercase and the rest lowercase
+filename=$(echo "$filename" | awk '{ for (i=1; i<=NF; i++) $i=tolower(substr($i,1,1)) substr($i,2) } 1' | tr '[:lower:]' '[:upper:]')
+
+# Create the final filename in the format: "$number. UPPERCASE(str).md"
+file_name="$number. $filename.md"
+
+# Create the contents of the file
+file_contents="$url\n$number"
+
+
+cd ~/IdeaProjects/competitive-coding/src/leetcode
+
+paddedNumber=$(printf "%04d" "$number")
 directory_name="problem$paddedNumber"
 mkdir $directory_name
 
 cd $directory_name
-echo "package leetcode.$directory_name; /** ques: tags: complexity: */ public class Solution{}" > Solution.java
-echo "package leetcode.$directory_name; import org.junit.jupiter.api.Test; public class SolutionTest{@Test public void testSolution() {}}" > SolutionTest.java
+echo -e "# $number. $filename\n\n* Question: $url \n* Solution: [here](Solution.java) \n* Testing: [here](SolutionTest.java) \n* Tags: \n* Complexity: " > "$file_name"
+echo "package leetcode.$directory_name; /** ques: $url tags: complexity: */ public class Solution{}" > Solution.java
+echo "package leetcode.$directory_name; import org.junit.jupiter.api.Test; public class SolutionTest{@Test public void testSolution1() {}}" > SolutionTest.java
