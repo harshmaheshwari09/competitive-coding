eb4f094014a15b4c18a5c676eeba20cd3b2b1325
Harsh Maheshwari <bittz09@gmail.com>
2023-07-10 23:14:07 +0100

    adding sol for problem #0226

---
+++ src/leetcode/java/problems/problem0226/226. INVERT BINARY TREE.md
@@ -0,0 +1,6 @@
+# 226. INVERT BINARY TREE
+
+* Question: https://leetcode.com/problems/invert-binary-tree/ 
+* Solution: [here](Solution.java) 
+* Tags: #tree
+* Complexity: Time: O(n), Space: O(1)
---
+++ src/leetcode/java/problems/problem0226/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.java.problems.problem0226;
+
+import com.sun.source.tree.Tree;
+import leetcode.java.model.TreeNode;
+
+/**
+ * Ques: https://leetcode.com/problems/invert-binary-tree/
+ * Tags: #tree
+ * Complexity: Time: O(n), Space: O(1)
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
