af3cd82fc43f91ea7addea298a3977bfdbd009c0
Harsh Maheshwari <bittz09@gmail.com>
2023-07-13 23:33:29 +0100

    adding sol for problem #0659

---
+++ src/leetcode/java/problems/problem0659/README.md
@@ -0,0 +1,6 @@
+# 659. ENCODE AND DECODE STRINGS
+
+* Question: https://leetcode.com/problems/encode-and-decode-strings/ 
+* Solution: [here](Solution.java) 
+* Tags: # 
+* Complexity: Time: O(), Space: O()
---
+++ src/leetcode/java/problems/problem0659/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.java.problems.problem0659;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ques: https://leetcode.com/problems/encode-and-decode-strings/
+ * Tags: #
+ * Complexity: Time: O(), Space: O()
+ */
+public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for (var str : strs) {
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') j++;
+
+            int length = Integer.valueOf(str.substring(i, j));
+            i = j + 1 + length;
+            list.add(str.substring(j + 1, i));
+        }
+        return list;
+    }
+}
