5dbe07a924c6c71b8fc2c8f9ecc415beda3a2d98
Harsh Maheshwari <bittz09@gmail.com>
2023-07-15 14:00:27 +0100

    updating 234

--- src/leetcode/java/problems/problem0234/Solution.java
+++ src/leetcode/java/problems/problem0234/Solution.java
@@ -9,18 +9,21 @@ import leetcode.java.model.ListNode;
  */
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        int mid = ListNode.getLength(head) / 2;
-        ListNode ptr1 = head;
-        while (mid-- > 0) {
-            ptr1 = ptr1.next;
+        if (head == null || head.next == null) {
+            return true;
         }
-        ptr1 = reverse(ptr1);
-        ListNode ptr2 = head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode ptr1 = head;
+        ListNode ptr2 = reverse(slow.next);
         return isSame(ptr1, ptr2);
     }
 
     private boolean isSame(ListNode ptr1, ListNode ptr2) {
-        while (ptr1 != null) {
+        while (ptr2 != null) {
             if (ptr1.val != ptr2.val) {
                 return false;
             }
@@ -36,7 +39,7 @@ public class Solution {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr  =next;
+            curr = next;
         }
         return prev;
     }
