d7e1f01a841a3360b1f82b07e10fb50e358f72ef
Harsh Maheshwari <bittz09@gmail.com>
2023-07-17 23:09:18 +0100

    adding sol for problem #0055

---
+++ src/leetcode/java/problems/problem0055/55. JUMP GAME.md
@@ -0,0 +1,6 @@
+# 55. JUMP GAME
+
+* Question: https://leetcode.com/problems/jump-game/ 
+* Solution: [here](Solution.java) 
+* Tags: #greedy
+* Complexity: Time: O(n), Space: O(1)
---
+++ src/leetcode/java/problems/problem0055/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.java.problems.problem0055;
+
+/**
+ * Ques: https://leetcode.com/problems/jump-game/
+ * Tags: #greedy
+ * Complexity: Time: O(n), Space: O(1)
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int last = nums.length - 1;
+        for (int idx = nums.length - 2; i >= 0; i++) {
+            if (idx + nums[idx] >= last) {
+                last = idx;
+            }
+        }
+        return last == 0;
+    }
+}
